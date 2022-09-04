37c289bd5bd08cf10f0baf8fd9aa6761cf8be4a0
samirazazy <samiralazazy@gmail.com>
2022-09-04 16:03:25 +0200

    fix

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
       useUnifiedTopology: true
     });
 
-    console.log('MongoDB Connected...');
+    console.log('MongoDB Connected 🎉🎉🎉');
   } catch (err) {
     console.error(err.message);
     // Exit process with failure
